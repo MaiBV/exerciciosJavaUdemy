f395c51c7c41784e75b141a2f85f64dc3b30dd25
MaiBV <maia.veras@gmail.com>
2024-11-04 18:08:12 -0300

    041124 18:08

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,230 @@
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
+	
+    public static void main(String[] args) {
+    	
+    	
+    }
+    	
+}
 
-	public static void main(String[] args) {
+    	// Exercício 07 for
+//    	Scanner sc = new Scanner(System.in);
+//
+//		int n = sc.nextInt();
+//		
+//		for (int i=1; i<=n; i++) {
+//
+//			int primeiro = i;
+//			int segundo = i * i;
+//			int terceiro = i * i * i;
+//			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+//		}
+//		
+//		sc.close();
+//	}
+//}
+
+
+    	// Exercício 06 for
+//    	Scanner sc = new Scanner(System.in);
+//    	
+//    	int n = sc.nextInt();
+//    	
+//    	for (int i=1; i<=n; i++) {
+//    		if (n % i == 0) {
+//    				System.out.println(i);
+//    			
+//    		}
+//    		
+//    	}
+//    	
+//    	
+//    	sc.close();
+//    }
+//}
+    	// Exercício 05 for
+//    	Scanner sc = new Scanner(System.in);
+//    	
+//    	int n = sc.nextInt();
+//    	
+//    	int fat = 1;
+//    	
+//    	for (int i=1; i<=n; i++) {
+//			fat = fat * i;
+//    	}
+//    	
+//    	System.out.println(fat);
+//    			
+//    	
+//    	sc.close();
+//    }
+//}
+        // Exercício 04 for
+//    	Locale.setDefault(Locale.US);
+//    	Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for (int i = 0; i < n; i++) {
+//            int x = sc.nextInt();
+//            int y = sc.nextInt();
+//
+//            if (y == 0) {
+//                System.out.println("Divisão impossível");
+//            } else {
+//                double divisao = (double) x / y;
+//                System.out.printf("%.1f%n", divisao);
+//            }
+//        }
+//
+//        sc.close();
+//    }
+//}
 		
+		// Exercício 03 for
+//		Locale.setDefault(Locale.US);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int n = 3;
+//				
+//		for(int i = 0; i < n; i ++) {
+//			
+//			double a = sc.nextDouble();
+//			double b = sc.nextDouble();
+//			double c = sc.nextDouble();
+//			
+//			double mp = (a * 2.0 + b * 3.0 + c * 5.0) / 10;
+//			System.out.printf("%.1f%n", mp);
+//			
+//		}
+//		
+//		
+//		sc.close();
+//	}
+//}
+
+
+		// Exercício 02 for
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int n = sc.nextInt();
+//		
+//		int in = 0;
+//		int out = 0;
+//		
+//		for (int i = 0; i < n; i++) {
+//			int x = sc.nextInt();
+//			if(x >= 10 && x <= 20) {
+//				in = in + 1;
+//				
+//			}
+//			else {
+//				out = out + 1;
+//			}
+//			
+//		}
+//			
+//		System.out.println(in + " in");
+//		System.out.println(out + " out");
+//		
+//		sc.close();
+//	}
+//}
+		
+		// Exercício 01 for
+//		Scanner sc = new Scanner(System.in);
+//				
+//		int x = sc.nextInt();
+//		int i;
+//		
+//		for (i = 1; i <= x; i ++ ) {
+//			if (i % 2 != 0) {
+//					System.out.println(i);
+//			}
+//		}
+//		
+//		sc.close();
+//	}
+//}
+
+		// Exercício 03 while
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int alcool = 0;
+//		int gasolina = 0;
+//		int diesel = 0;
+//		
+//		int codigo = sc.nextInt();
+//		
+//		while (codigo != 4) {
+//			if (codigo == 1) {
+//					alcool = alcool + 1;
+//			}
+//			else if (codigo == 2) {
+//					gasolina = gasolina + 1;
+//			}
+//			else if (codigo == 3) {
+//					diesel = diesel + 1;
+//			}
+//			
+//			codigo = sc.nextInt();
+//		}
+//		System.out.println("MUITO OBRIGADA");
+//		System.out.println("Álcool: " + alcool);
+//		System.out.println("Gasolina: " + gasolina );
+//		System.out.println("Diesel: " + diesel);
+//		
+//		sc.close();
+//	}
+//}
+
+
+		// Exercício 02 while
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int x = sc.nextInt();
+//		int y = sc.nextInt();
+//			
+//		while (x != 0 && y != 0) {
+//			if (x > 0 && y > 0) {
+//				System.out.println("primeiro");
+//			}
+//			else if (x < 0 && y > 0) {
+//				System.out.println("segundo");
+//			}
+//			else if (x < 0 && y < 0) {
+//				System.out.println("terceiro");
+//			}
+//			else {
+//				System.out.println("quarto");
+//			}
+//			x = sc.nextInt();
+//			y = sc.nextInt();
+//			
+//			
+//		}
+//		
+//		sc.close();
+//	}
+//}
+		
+		// Exercício 01 while
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int senha = sc.nextInt();
+//		
+//		while (senha != 2002) {
+//				System.out.println("Senha Inválida");			
+//				senha = sc.nextInt();
+//		}
+//		
+//		System.out.println("Acesso Permitido");
+//			
+//		
+//	sc.close();	
+//	}
+//}
+	
+
 		// Exercício 08 Condicional
 //		Locale.setDefault(Locale.US);
 //		Scanner sc = new Scanner(System.in);
@@ -313,6 +533,6 @@ public class Main {
 //				
 //		sc.close();
 		
-	}
+//	}
 
-}
+//}
